6b326841ef102df0cb5ce251589d0261fe2ef9db
oweson <570347720@qq.com>
2020-05-24 16:09:41 +0800

    Initial commit

---
+++ src/main/java/com/example/pdftmp/PdfTmpApplication.java
@@ -0,0 +1,13 @@
+package com.example.pdftmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfTmpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdfTmpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pdftmp/PdfTmpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pdftmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfTmpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
